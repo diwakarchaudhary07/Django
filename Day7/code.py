cd55a12bdc7b6ca5bd8f0784327fe4a7f3356ff1
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-08 23:56:58 +0530

    add

---
+++ Day7/code.py
@@ -0,0 +1,25 @@
+# ✅ Day  7 :- Find Code In Webpage
+
+# How to find the code of any webpage?
+
+# Step 1 :- First Have To Visit exactly on that web page and copy the unique url.
+# Like here we copied categorie:- 
+# http://127.0.0.1:8000/categorie
+
+# Step 2 :- Now We have to visit VSCode And Search for urls.py by using ctrl + P Shortcut and make sure this is the app url not project url.
+# ctrl + p #urls.py
+
+# Step 3:- in urls.py we have to search categorie word by using ctrl + f short cut
+# ctrl + f #categorie
+
+# Step 4:- In urls.py there are three parts of an url
+#               First one is url pattern name 
+#               second function name 
+#               Third url name 
+
+# So we have to click on the function name by using ctrl + click. 
+# It redirects to the exact function which is in views.py. In that function we can see exactly the HTML file name.
+
+# So this is how you can find the exact HTML code of that webpage.
+
+
