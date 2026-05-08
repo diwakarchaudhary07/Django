17f9bafae0f0879efca5a7b507045a47c09b14c9
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-08 23:59:15 +0530

    add

---
+++ Day12/code.py
@@ -0,0 +1,12 @@
+# ✅ Day  12 :- CRUD Operation Update And Delete.
+
+#   <form method="post" enctype="multipart/form-data">
+#      <!-- we have to use enctype="multipart/form-data" Because we have to store multiple type of data by using this form. -->
+#        {% csrf_token %}
+#         <!-- <input type="hidden" name="csrfmiddlewaretoken" value="QX0iaE2Lv17yf1kcaKPIaz2SL2mj3iFDcdAobx6ccQUmj6lm3tIYWHtiGuezcBze"> -->
+#         <!-- cross site request forgery token is used to prevent cross site request forgery attack.
+#         It is a security measure to protect against malicious attacks that can occur when a user is tricked into submitting a form from another website.
+#         By including the {% csrf_token %} in the form, Django generates a unique token for each form submission, which helps to ensure that the request is coming from a trusted source and not from an attacker.
+#         This token is then validated on the server side when the form is submitted, providing an additional layer of security for the application. -->
+       
+
