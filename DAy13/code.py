7c2e4398f7362606b76cd14559efa8d3374cd144
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-09 22:42:23 +0530

    add

---
+++ DAy13/code.py
@@ -0,0 +1,23 @@
+# ✅ Day  13 :-SMTP AND AUTOMATED MAIL.
+
+# Two Questions To Explore:-
+# How To Send Email In Django ?
+# What Is Soft Delete And Hard Delete ?
+
+
+# Firstly we have to create a login and sign up page with role Coolum
+#        In which role will be assigned by admin side by default a user will be created as a student.
+
+# Register form fields 
+#        Full name 
+#        Email
+#        Password
+#        Conform Password
+  
+# Whenever a new user registers or creates a profile successfully there is a welcome message will be sent to the user email.
+
+# a student can see his own profile and edit his profile and a teacher can add student delete student make active and inactive student.
+
+
+# YT VIDEO LINK :- How To Set Up Gmail SMTP Server - Full Guide
+
