662ab86b19f551078b60bcc3ea89cd4457ab2aef
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-09 22:42:33 +0530

    add

---
+++ Day14/code.py
@@ -0,0 +1,36 @@
+# ✅ Day  14 :- Virtual Enviroment Setup
+
+
+# Step 1:- Run This Command For Installing Virtual Environment 
+#          (python -m venv myenv)
+
+
+# Step 2:- Run THis COmmand For Activation
+#          For Windows (myenv\Scripts\activate)
+#          For Mac/Linux(source myenv/bin/activate)
+
+
+# Step 3:- Now You Have To Install Django 
+#          (pip install django)
+
+
+# Step 4:- Now Run (pip install pscopyg2) For Connecting Database
+
+
+# Step 5:- Now Run Two Command
+#          (python manage.py makemigrations)
+#          (python manage.py migrate)
+
+
+# Step 6:- Run This Command For Image Storage
+#          (pip install pillow)
+
+
+# Step 7:- Create Super User
+#          (python manage.py createsuperuser)
+
+
+
+
+# H.W :- Make A New Advance CRUD Project In Virtual Environment
+ 
